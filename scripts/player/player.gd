de38c63fb2f0c66176f82d888122747cac68a7e2
Magnus Marcuslund <magnusras2@gmail.com>
2025-10-28 17:01:55 +0100

    Refactor(player): replaced input_state with individuel functions.

--- scripts/player/player.gd
+++ scripts/player/player.gd
@@ -2,28 +2,34 @@ extends CharacterBody2D
 
 @export var move_speed = 300
 @onready var _animated_sprite = $AnimatedSprite2D
-var input_state
+var move_vector
+var attack_input
+var block_input
 var last_vector = Vector2.RIGHT
 var mouse_sector
-var move_vector
 var directions = ["e", "se", "s", "sw", "w", "nw", "n", "ne"]
-var mouse_direction = directions[mouse_sector]
+var mouse_direction = 0
 var is_attacking = false
 
 
-func get_input():
-	input_state = {
-		"move_vector": Input.get_vector("move_left", "move_right", "move_up", "move_down"),
-		"attack": Input.is_action_just_pressed("attack"),
-		"block": Input.is_action_pressed("block"),
-		"roll": Input.is_action_pressed("roll"),
-	}
-	return input_state
+func get_move_vector():
+	move_vector = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	return move_vector
+
+
+func get_attack_input():
+	attack_input = Input.is_action_just_pressed("attack")
+	return attack_input
+
+
+func get_block_input():
+	block_input = Input.is_action_pressed("block")
+	return block_input
 
 
 func calculate_velocity():
 	if !is_attacking:
-		velocity = input_state["move_vector"] * move_speed
+		velocity = get_move_vector() * move_speed
 	else:
 		velocity = Vector2.ZERO
 
@@ -38,7 +44,7 @@ func get_mouse_sector():
 
 
 func attack_animation():
-	if input_state["attack"] && !is_attacking:
+	if get_attack_input() && !is_attacking:
 		is_attacking = true
 		if last_vector.x > 0.8:
 			_animated_sprite.play("attack_e")
@@ -59,7 +65,7 @@ func attack_animation():
 
 
 func idle_animation():
-	if !input_state["move_vector"]:
+	if !get_move_vector():
 		if last_vector.x > 0.8:
 			_animated_sprite.play("idle_e")
 		elif last_vector.x < 0.8 && last_vector.x > 0 && last_vector.y > 0:
@@ -79,27 +85,27 @@ func idle_animation():
 
 
 func run_animation():
-	if input_state["move_vector"].x > 0.8:
+	if get_move_vector().x > 0.8:
 		_animated_sprite.play("run_e")
-	elif input_state["move_vector"].x < 0.8 && input_state["move_vector"].x > 0 && input_state["move_vector"].y > 0:
+	elif get_move_vector().x < 0.8 && get_move_vector().x > 0 && get_move_vector().y > 0:
 		_animated_sprite.play("run_se")
-	elif input_state["move_vector"].x == 0 && input_state["move_vector"].y > 0.8:
+	elif get_move_vector().x == 0 && get_move_vector().y > 0.8:
 		_animated_sprite.play("run_s")
-	elif input_state["move_vector"].x < 0 && input_state["move_vector"].x > -0.8 && input_state["move_vector"].y > 0:
+	elif get_move_vector().x < 0 && get_move_vector().x > -0.8 && get_move_vector().y > 0:
 		_animated_sprite.play("run_sw")
-	elif input_state["move_vector"].x < -0.8:
+	elif get_move_vector().x < -0.8:
 		_animated_sprite.play("run_w")
-	elif input_state["move_vector"].x < 0 && input_state["move_vector"].x > -0.8 && input_state["move_vector"].y < 0:
+	elif get_move_vector().x < 0 && get_move_vector().x > -0.8 && get_move_vector().y < 0:
 		_animated_sprite.play("run_nw")
-	elif input_state["move_vector"].x == 0 && input_state["move_vector"].y < -0.8:
+	elif get_move_vector().x == 0 && get_move_vector().y < -0.8:
 		_animated_sprite.play("run_n")
-	elif input_state["move_vector"].x > 0 && input_state["move_vector"].x < 0.8 && input_state["move_vector"].y < 0:
+	elif get_move_vector().x > 0 && get_move_vector().x < 0.8 && get_move_vector().y < 0:
 		_animated_sprite.play("run_ne")
 
 
 func update_animation():
-	if input_state["move_vector"]:
-		last_vector = input_state["move_vector"]
+	if get_move_vector():
+		last_vector = get_move_vector()
 	attack_animation()
 	if is_attacking:
 		print("is attacking")
@@ -108,15 +114,15 @@ func update_animation():
 	run_animation()
 
 
-func _physics_process(_delta):
-	get_input()
-	get_mouse_sector()
-	calculate_velocity()
-	update_animation()
-	move_and_slide()
-
-
 func animation_done() -> void:
 	print("signal received")
 	if _animated_sprite.animation.begins_with("attack"):
 		is_attacking = false
+
+
+func _physics_process(_delta):
+	get_move_vector()
+	get_mouse_sector()
+	calculate_velocity()
+	update_animation()
+	move_and_slide()
